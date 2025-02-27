3bdd183ae1c69c8f2378ccd8e22563dda336d53b
Gul-yilmaz <yilmazlutfiyegul@gmail.com>
2025-02-27 08:55:26 +0300

    image yolu hatası cözüldü

--- lib/widgets/image.dart
+++ lib/widgets/image.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Myimages extends StatelessWidget {
+  //sabit arayüz
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //sayfanın iskelesi
       body: Center(
+        //ortalar
         child: ClipRRect(
+          //köseleri yuvarlar
           borderRadius: BorderRadius.circular(20),
           child: Container(
             height: 300,
@@ -13,7 +17,7 @@ class Myimages extends StatelessWidget {
             color: Colors.blue,
             child: Image.asset(
               'lib/widgets/images/maria.jpg',
-              fit: BoxFit.fill,
+              fit: BoxFit.fill, // resmi dolduruyor
             ),
           ),
         ),
